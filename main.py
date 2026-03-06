70128835b007d3dd8f6f2c50546785474c27cd47
THOMÁS GAJEWSKI STOIANOV <54532962838@info.local>
2026-03-06 20:16:10 -0300

    subindo feat-variancia

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
 
-def limpar_dados () :
+def limpar_dados (dados_sujos) :
     pass
 
 def calcular_media () :
@@ -10,8 +10,22 @@ def calcular_media () :
 def calcular_mediana () :
     pass
 
-def calcular_variancia () :
-    pass
+def calcular_variancia (dados_sujos) :
+    dados = []
+
+    for valor in dados_sujos:
+        if type(valor) == int or type(valor) == float:
+            dados.append(valor)
+
+    media = sum(dados) / len(dados)
+
+    soma = 0
+    for x in dados:
+        soma += (x - media) ** 2
+    
+    variancia = soma / len(dados)
+
+    return variancia
 
 def obter_extremos () :
     pass
@@ -19,3 +33,8 @@ def obter_extremos () :
 dados = limpar_dados ( dados_sujos )
 
 print ( f" Dados processados : { dados }")
+
+variancia = calcular_variancia(dados_sujos)
+print("Variancia: ", variancia)
+
+print("Verificado por: Thomás Stoianov")
