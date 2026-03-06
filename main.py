2d3820872c11db291b220bb254cd739af9fb4c7b
Felipe Polegato <felipepolegatofr@gmail.com>
2026-03-06 20:05:49 -0300

    media

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
 def limpar_dados () :
     pass
 
-def calcular_media () :
-    pass
+def calcular_media(dados_limpos): #Colocar a lista (dados_limpos)
+    soma = sum(dados_limpos)
+    quantidade = len(dados_limpos)
+    media = soma / quantidade
+    return media
 
 def calcular_mediana () :
     pass
