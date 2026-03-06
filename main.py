1bf628f0a1512df8d524deccb99955be28ae8f16
Gustavo-Rodrigue <47822733800@info.local>
2026-03-06 19:40:11 -0300

    Criação do arquivo inicial

---
+++ main.py
@@ -0,0 +1,21 @@
+
+dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
+
+def limpar_dados () :
+    pass
+
+def calcular_media () :
+    pass
+
+def calcular_mediana () :
+    pass
+
+def calcular_variancia () :
+    pass
+
+def obter_extremos () :
+    pass
+
+dados = limpar_dados ( dados_sujos )
+
+print ( f" Dados processados : { dados }")
